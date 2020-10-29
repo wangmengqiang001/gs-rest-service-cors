2570745373d31bdf7386d4715a06a69b648fe282
wangmengqiang <wangmengqiang001@126.com>
2020-10-29 17:31:52 +0800

    add service registry and remove cors setting
    Signed-off-by: wangmengqiang <wangmengqiang001@126.com>

--- complete/src/main/java/com/example/restservicecors/GreetingController.java
+++ complete/src/main/java/com/example/restservicecors/GreetingController.java
@@ -2,7 +2,7 @@ package com.example.restservicecors;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +14,7 @@ public class GreetingController {
 
 	private final AtomicLong counter = new AtomicLong();
 
-	@CrossOrigin(origins = "http://localhost:8080")
+	//@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(required = false, defaultValue = "World") String name) {
 		System.out.println("==== get greeting ====");
